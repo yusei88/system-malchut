b1e15c20d6aa40e79095a46a29d80b108aa72509
yusei88 <yuseisato1@gmail.com>
2024-03-20 15:46:26 +0900

    feature: add flavor

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-);
-  runApp(const MainApp());
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  PackageInfo packageInfo = await PackageInfo.fromPlatform();
+  runApp(MainApp(packageInfo: packageInfo,));
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  final PackageInfo packageInfo;
+  const MainApp({super.key, required this.packageInfo});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    String appName = packageInfo.appName;
+    String packageName = packageInfo.packageName;
+    String version = packageInfo.version;
+    String buildNumber = packageInfo.buildNumber;
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              children: [
+                const Text('Hello World!'),
+                Text('appName: $appName'),
+                Text('packageName: $packageName'),
+                Text('version: $version'),
+                Text('buildNumber: $buildNumber'),
+              ],
+            ),
+          ),
         ),
       ),
     );
